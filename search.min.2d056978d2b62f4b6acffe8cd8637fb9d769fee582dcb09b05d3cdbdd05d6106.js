7f016e6305384b2b85ad661961b3a1f835cb79b3
Kovas Palunas <kovas.palunas@gmail.com>
2020-03-18 23:11:05 -0700

    rebuilding site Wed 18 Mar 2020 11:11:05 PM PDT

---
+++ search.min.2d056978d2b62f4b6acffe8cd8637fb9d769fee582dcb09b05d3cdbdd05d6106.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/search-data.min.ea6b15910a00ef05c0ec8f5708668cc9a56350195504ce230d22e4c9e7e67880.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
