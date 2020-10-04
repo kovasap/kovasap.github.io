1f75887658dbd367a33c6e63a41a9022b1384897
Kovas Palunas <kovas.palunas@gmail.com>
2020-10-03 21:05:55 -0700

    removed obselete code

--- perlin_flow/cljs-out/dev-main.js
+++
@@ -1,9 +0,0 @@
-var CLOSURE_UNCOMPILED_DEFINES = {};
-var CLOSURE_NO_DEPS = true;
-if(typeof goog == "undefined") document.write('<script src="/perlin_flow/cljs-out/dev/goog/base.js"></script>');
-document.write('<script src="/perlin_flow/cljs-out/dev/goog/deps.js"></script>');
-document.write('<script src="/perlin_flow/cljs-out/dev/cljs_deps.js"></script>');
-document.write('<script>if (typeof goog == "undefined") console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?");</script>');
-document.write('<script>goog.require("devtools.preload");</script>');
-document.write('<script>goog.require("process.env");</script>');
-document.write('<script>goog.require("sketches.perlin_flow");</script>');
